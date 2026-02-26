baaf95d185c0ffa1b7a2cde55c24a1f7f78736d1
Raphael Mischiatti <r4faelsouza09@gmail.com>
2026-02-25 21:23:49 -0300

    atualizacao para maquina de lavar

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,28 +1,37 @@
 package br.com.fiapride.main;
 
-// Importamos a classe Passageiro para que o sistema a reconheça
-import br.com.fiapride.model.Passageiro;
+// Importamos a classe MaquinaDeLavar para que o sistema a reconheça
+import br.com.fiapride.model.MaquinaDeLavar;
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
         // INSTANCIAÇÃO
         // O comando 'new' aloca memória para um novo objeto.
-        // Criando o primeiro passageiro (Objeto 1)
-        Passageiro passageiro1 = new Passageiro();
-        passageiro1.nome = "Ana Silva";
-        passageiro1.saldo = 50.0;
+        // Criando a primeira maquina e seguindo com novos exemplos (Objeto 1)
+        MaquinaDeLavar maquina1 = new MaquinaDeLavar();
+        maquina1.MaterialCorpo = "Aço";
+        maquina1.Voltagem = 110;
+        maquina1.PesoDaMaquina = 45.6;
 
-        // Criando o segundo passageiro (Objeto 2)
-        Passageiro passageiro2 = new Passageiro();
-        passageiro2.nome = "Raphael Souza";
-        passageiro2.saldo = 12.50;
+        MaquinaDeLavar maquina2 = new MaquinaDeLavar();
+        maquina2.MaterialCorpo = "Plastico";
+        maquina2.Voltagem = 220;
+        maquina2.PesoDaMaquina = 60.5;
+
+        MaquinaDeLavar maquina3 = new MaquinaDeLavar();
+        maquina3.MaterialCorpo = "Inox";
+        maquina3.Voltagem = 110;
+        maquina3.PesoDaMaquina = 55.2;
+
+
 
         // Exibindo os dados no Console
         System.out.println("--- Sistema FiapRide ---");
-        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo);
-        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo);
-        
-        // Teste mental: Se eu mudar o nome do passageiro1, o passageiro2 muda?
+        System.out.println("Material da Maquina: " + maquina1.MaterialCorpo + " | Voltagem da Maquina: " + maquina1.Voltagem + "V | Peso da Maquina: " + maquina1.PesoDaMaquina);
+        System.out.println("Material da Maquina: " + maquina2.MaterialCorpo + " | Voltagem da Maquina: " + maquina2.Voltagem + "V | Peso da Maquina: " + maquina2.PesoDaMaquina);
+        System.out.println("Material da Maquina: " + maquina3.MaterialCorpo + " | Voltagem da Maquina: " + maquina3.Voltagem + "V | Peso da Maquina: " + maquina3.PesoDaMaquina);
+        // Oq falta para evoluir esse codigo?
+        // Uma identificacao de qm e dono de cada maquina talvez 
     }
 }
--- src/br/com/fiapride/model/MaquinaDeLavar.java
+++ src/br/com/fiapride/model/MaquinaDeLavar.java
@@ -1,5 +1,10 @@
 package br.com.fiapride.model;
 
 public class MaquinaDeLavar {
-
+    // Atributos (Características)
+    public String MaterialCorpo;
+    public double PesoDaMaquina;
+    public int Voltagem; 
+    // Nota: Por enquanto usamos 'public' para facilitar o aprendizado.
+    // Nas próximas aulas, aprenderemos a proteger esses dados.
 }
